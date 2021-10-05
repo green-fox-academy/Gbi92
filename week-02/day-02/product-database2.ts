58d38533d6efe6e94282c3386e4599107b050102
Gabriella Koos <koos.gabriella92@gmail.com>
2021-10-05 20:29:09 +0200

    created product-database2.ts to do exercise later

---
+++ week-02/day-02/product-database2.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+let someProduct: any = {
+  Eggs: 200,
+  Milk: 200,
+  Fish: 400,
+  Apples: 150,
+  Bread: 50,
+  Chicken: 550,
+};
+
+//Which products cost less than 201? (just the name)
+
+//Which products cost more than 150? (name + price)

b18b048c0be7427a753e73b9d4d9eed03f314448
Gabriella Koos <koos.gabriella92@gmail.com>
2021-10-07 15:49:33 +0200

    finished 2 projects

---
+++ week-02/day-04/envelope-star/envelope-star.ts
@@ -0,0 +1,40 @@
+"use strict";
+
+const canvas = document.querySelector(".main-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+function drawLine(
+  xStart: number,
+  yStart: number,
+  xTo: number,
+  yTo: number
+): void {
+  ctx.beginPath();
+  ctx.strokeStyle = "green";
+  ctx.moveTo(xStart, yStart);
+  ctx.lineTo(xTo, yTo);
+  ctx.stroke();
+}
+
+let height: number = canvas.height;
+let middle: number = height / 2;
+let gap: number = 10;
+
+for (let y: number = middle - gap, x: number = height - gap; y >= 0; y -= gap, x -= gap) {
+  drawLine(middle, y, x, 1 / x + middle);
+}
+
+for (let y: number = middle + gap, x: number = height - gap; y <= height; y += gap, x -= gap) {
+  drawLine(middle, y, x, middle - 1 / x);
+}
+
+for (let y: number = middle - gap, x: number = gap; y > 0; y -= gap, x += gap) {
+    drawLine(middle, y, x, middle + 1 / x);
+}
+
+for (let y: number = middle + gap, x: number = gap; y < height; y += gap, x += gap) {
+  drawLine(middle, y, x, middle + 1 / x);
+}
---
+++ week-02/day-04/line-play/line-play.ts
@@ -0,0 +1,27 @@
+"use strict";
+
+const canvas = document.querySelector(".main-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+function drawLine(xStart: number, yStart: number, xTo: number, yTo: number): void {
+  ctx.beginPath();
+  ctx.moveTo(xStart, yStart);
+  ctx.lineTo(xTo, yTo);
+  ctx.stroke();
+}
+
+let height: number = canvas.height;
+let gap: number = 20;
+
+for (let i: number = height - gap; i > 0; i -= gap) {
+    ctx.strokeStyle = 'green';
+    drawLine(0, i, i, height - (1 / i));
+}
+
+for (let j: number = height - gap; j > 0; j -= gap) {
+    ctx.strokeStyle = 'purple';
+    drawLine(j, 0, height - (1 / j), j);
+}

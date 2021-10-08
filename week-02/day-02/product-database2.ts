f162c7f936ecd390cba8485a1a6d9620d4595c0d
Gabriella Koos <koos.gabriella92@gmail.com>
2021-10-08 09:57:47 +0200

    finished product-database2

--- week-02/day-02/product-database2.ts
+++ week-02/day-02/product-database2.ts
@@ -10,5 +10,19 @@ let someProduct: any = {
 };
 
 //Which products cost less than 201? (just the name)
+let lessThan201: string[] = [];
+
+Object.keys(someProduct).forEach((key) => {
+  if (someProduct[key] < 201) {
+    lessThan201.push(key);
+  }
+});
+
+console.log(`Less than 201: ${lessThan201}`);
 
 //Which products cost more than 150? (name + price)
+Object.keys(someProduct).forEach((key) => {
+  if (someProduct[key] > 150) {
+    console.log(`More than 150: ${key} - ${someProduct[key]}`);
+  }
+});

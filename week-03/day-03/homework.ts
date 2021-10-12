c7ff22df31807b564d91130f83cb418093f3915f
Gabriella Koos <koos.gabriella92@gmail.com>
2021-10-12 19:02:12 +0200

    finished homework for day03

---
+++ week-03/day-03/homework.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+// # Doable homework
+
+// - Create 5 trees
+//   - Store the data of them in variables in your program
+//   - for every tree the program should store its'
+//     - type
+//     - leaf color
+//     - age
+//     - sex
+//   - you can use just variables, or lists and/or maps
+
+let trees: any[] = [
+  { type: 'oak', leafColor: 'green', age: 120, sex: 'F' },
+  { type: 'maple oak', leafColor: 'red', age: 150, sex: 'F' },
+  { type: 'pine', leafColor: 'evergreen', age: 70, sex: 'M' },
+  { type: 'palm', leafColor: 'dark green', age: 40, sex: 'M' },
+  { type: 'willow', leafColor: 'light green', age: 28, sex: 'F' },
+];

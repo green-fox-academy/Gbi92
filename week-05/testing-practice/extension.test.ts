082933ca218e689f3044c94e3aa086a4b385cfb1
Gabriella Koos <koos.gabriella92@gmail.com>
2021-10-26 17:03:39 +0200

    started testing exercises

---
+++ week-05/testing-practice/extension.test.ts
@@ -0,0 +1,59 @@
+'use strict';
+
+import test from 'tape';
+import { add, maxOfThree, median, isVowel, translate } from './extension';
+
+test('add: 3 and 3 is 6', function (t: any): any {
+  t.equal(add(3, 3), 6);
+  t.end();
+});
+
+test('add: -1 and 4 is 3', function (t: any): any {
+  t.equal(add(-1, 4), 3);
+  t.end();
+});
+
+test('max of three: 8', function (t: any): any {
+  t.equal(maxOfThree(5, 4, 8), 8);
+  t.end();
+});
+
+test('max of three: 5', function (t: any): any {
+  t.equal(maxOfThree(3, 4, 5), 5);
+  t.end();
+});
+
+test('median: four', function (t: any): any {
+  t.equal(median([7, 5, 3, 5]), 5);
+  t.end();
+});
+
+test('median: five', function (t: any): any {
+  t.equal(median([1, 2, 3, 4, 5]), 3);
+  t.end();
+});
+
+test('is vowel: a', function (t: any): any {
+  t.ok(isVowel('a'));
+  t.end();
+});
+
+test('is vowel: u', function (t: any): any {
+  t.ok(isVowel('u'));
+  t.end();
+});
+
+test('is vowel: k', function(t: any): any {
+  t.notOk(isVowel('k'));
+  t.end();
+});
+
+test('translate: bemutatkozik', function (t: any): any {
+  t.equal(translate('bemutatkozik'), 'bevemuvutavatkovozivik');
+  t.end();
+});
+
+test('translate: lagopus', function (t: any): any {
+  t.equal(translate('lagopus'), 'lavagovopuvus');
+  t.end();
+});
---
+++ week-05/testing-practice/extension.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function maxOfThree(a: number, b: number, c: number): number {
+  return Math.max(a, b, c);
+};
+
+export function median(pool: number[]): number {
+  return pool[Math.floor((pool.length - 1) / 2)];
+}
+
+export function isVowel(character: string): boolean {
+  return ['a', 'u', 'o', 'e', 'i'].some(vowel => vowel === character);
+}
+
+export function translate(hungarian: string): string {
+  let teve = hungarian;
+  let length = teve.length;
+
+  for (let i = 0; i < length; i++) {
+    let c = teve[i];
+    if (isVowel(c)) {
+      teve = teve.split(c).join(`${c}v${c}`);
+      i += 2;
+      length += 2;
+    }
+  }
+  return teve;
+}
+
+// Check out the folder. There's a work file and a test file.
+
+//  -  Run the tests, all 11 should be green (passing).
+//  -  The implementations though are not quite good.
+//  -  Create tests that'll fail, and will show how the implementations are wrong(You can assume that the implementation of join and split are good)
+//  -  After creating the tests, fix the implementations
+//  -  Check again, if you can create failing tests
+//  -  Implement if needed

5c0fe551f69cc27c1d08cf471dbe6d588514afcb
Gabriella Koos <koos.gabriella92@gmail.com>
2021-10-28 16:32:29 +0200

    created tic-tac-toe files

---
+++ week-05/fileIO-practice/tic-tac-toe.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Write a function that takes a filename as a parameter
+// The file contains an ended Tic-Tac-Toe match
+// We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
+// Return "X", "O" or "Draw" based on the input file's content
+
+function ticTacResult(path:string) {
+    
+}
+
+console.log(ticTacResult('win-o.txt'))
+// Should print "O"
+
+console.log(ticTacResult('win-x.txt'))
+// Should print "X"
+
+console.log(ticTacResult('draw.txt'))
+// Should print "Draw"

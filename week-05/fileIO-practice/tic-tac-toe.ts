7ee3a66f7297aa37e8a83747dab5ca18608708b6
Gabriella Koos <koos.gabriella92@gmail.com>
2021-10-28 17:27:41 +0200

    started tictactoe exercise

--- week-05/fileIO-practice/tic-tac-toe.ts
+++ week-05/fileIO-practice/tic-tac-toe.ts
@@ -1,19 +1,33 @@
 'use strict';
 
+export {};
+
 // Write a function that takes a filename as a parameter
 // The file contains an ended Tic-Tac-Toe match
 // We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
 // Return "X", "O" or "Draw" based on the input file's content
 
-function ticTacResult(path:string) {
-    
+const fs = require('fs');
+
+function readFile(path: string): string {
+  try {
+    return fs.readFileSync(path, 'utf-8');
+  } catch (error) {
+    throw 'File does not exist';
+  }
 }
 
-console.log(ticTacResult('win-o.txt'))
+function ticTacResult(path: string) {
+  let fileContent: string = readFile(path);
+  let lines: string[] = fileContent.split('\n');
+  
+}
+
+console.log(ticTacResult('win-o.txt'));
 // Should print "O"
 
-console.log(ticTacResult('win-x.txt'))
+console.log(ticTacResult('win-x.txt'));
 // Should print "X"
 
-console.log(ticTacResult('draw.txt'))
+console.log(ticTacResult('draw.txt'));
 // Should print "Draw"

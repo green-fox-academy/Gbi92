95814bdc2f125932fc997b8f022620e929649fd5
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-11 16:57:05 +0100

    finished cocktails exercise

---
+++ week-06/day-04/cocktails/index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use('/static', express.static('static'));
+app.set('view engine', 'ejs');
+
+const cocktails = [
+  {
+    name: 'GIN FIZZ',
+    price: 1520,
+    contains: ['gin', 'sugar', 'lemon juice', 'soda'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'BLOODY MARY',
+    price: 1650,
+    contains: ['vodka', 'tomato juice', 'spices'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'SEX ON THE BEACH',
+    price: 1850,
+    contains: [
+      'vodka',
+      'peach schnapps',
+      'orange juice',
+      'cranberry juice'
+    ],
+    isAlcoholic: true,
+  },
+  {
+    name: 'CUBA LIBRE',
+    price: 1850,
+    contains: ['rum', 'cola', 'lime juice'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'MOJITO',
+    price: 1850,
+    contains: ['rum', 'sugar', 'lime juice', 'soda water'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'LONG ISLAND ICE TEA',
+    price: 2450,
+    contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'],
+    isAlcoholic: true,
+  },
+  {
+    name: 'VIRGIN MOJITO',
+    price: 990,
+    contains: ['sugar', 'lime juice', 'soda water'],
+    isAlcoholic: false,
+  },
+  {
+    name: 'SAFE SEX ON THE BEACH',
+    price: 990,
+    contains: ['peach schnapps', 'orange juice', 'cranberry juice'],
+    isAlcoholic: false,
+  },
+];
+
+const alcoholList = ['gin', 'vodka', 'rum', 'tequila'];
+
+app.get('/', (req, res) => {
+  if(req.query.alcohol === undefined) {
+    res.render('home', {
+      cocktails: cocktails,
+      alcoholList: alcoholList
+    });
+  } else {
+    const filtered = cocktails.filter(el => el.contains.includes(req.query.alcohol));
+      res.render('home', {
+        cocktails: filtered,
+        alcoholList: alcoholList
+      });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});

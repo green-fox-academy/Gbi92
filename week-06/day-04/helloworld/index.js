6d77189692b39d26c6f6ce8cd485d6063291fd6f
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-11 14:08:07 +0100

    finsihed hello world app

---
+++ week-06/day-04/helloworld/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+  res.render('home', {
+      title: 'Hello World',
+      name: 'Gbi'
+  });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});

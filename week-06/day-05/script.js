256cc7b7fbcc6363c6e92e2ca4846653f02cdba0
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-12 13:43:00 +0100

    thumbnail click works

--- week-06/day-05/script.js
+++ week-06/day-05/script.js
@@ -1,46 +1,49 @@
 'use strict';
 
-let currentPhoto = 0;
-
 let imagesData = [
     {
         photo: './images/img0.jpg',
-        title: 'Lago di Braies',
+        title: 'Lorem',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     },
     {
         photo: './images/img1.jpg',
-        title: 'Sunset in the Mountains',
+        title: 'Lorem',
         description: 'Diam sollicitudin tempor id eu nisl. Congue quisque egestas diam in.'
     },
     {
         photo: './images/img2.jpg',
-        title: 'Sunrise',
+        title: 'Lorem',
         description: 'Nibh venenatis cras sed felis eget. Sit amet purus gravida quis.'    
     },
     {
         photo: './images/img3.jpg',
-        title: 'Sunset on the Canal',
+        title: 'Lorem',
         description: 'Dignissim enim sit amet venenatis urna cursus eget nunc. Nibh ipsum consequat nisl vel.'
     },
     {
         photo: './images/img4.jpg',
-        title: 'Bridge to Where the Wild Things Are',
+        title: 'Lorem',
         description: 'Nunc id cursus metus aliquam eleifend mi. Pellentesque habitant morbi tristique senectus.'
     },
     {
         photo: './images/img5.jpg',
-        title: 'Poppy field',
+        title: 'Lorem',
         description: 'Porta non pulvinar neque laoreet suspendisse. Amet tellus cras adipiscing enim.'
     },
     {
         photo: './images/img6.jpg',
-        title: 'Hay field',
+        title: 'Lorem',
         description: 'Placerat duis ultricies lacus sed turpis tincidunt id. Commodo viverra maecenas accumsan lacus vel facilisis volutpat.'
     },
     {
         photo: './images/img7.jpg',
-        title: 'A Walk in the Snow',
+        title: 'Lorem',
+        description: 'Fringilla phasellus faucibus scelerisque eleifend donec pretium. Non nisi est sit amet facilisis.'
+    },
+    {
+        photo: './images/img8.jpg',
+        title: 'Lorem',
         description: 'Fringilla phasellus faucibus scelerisque eleifend donec pretium. Non nisi est sit amet facilisis.'
     }
 ];
@@ -48,6 +51,9 @@ let imagesData = [
 const photo = document.querySelector('.chosen-photo');
 const photoTitle = document.querySelector('.title');
 const photoDescr = document.querySelector('.description');
+const rightArrow = document.querySelector('.nav-right');
+const leftArrow = document.querySelector('.nav-left');
+const thumbContainer = document.querySelector('#thumbnail-container');
 
 function loadPhoto(photoNumber) {
   photo.setAttribute('src', imagesData[photoNumber].photo);
@@ -55,11 +61,12 @@ function loadPhoto(photoNumber) {
   photoDescr.textContent = imagesData[photoNumber].description;
 }
 
+let currentPhoto = 0;
+
+//onLoad:
 loadPhoto(currentPhoto);
 
-const rightArrow = document.querySelector('.nav-right');
-const leftArrow = document.querySelector('.nav-left');
-
+//arrow clicks:
 leftArrow.onclick = () => {  
     if(currentPhoto == 0 ){
         currentPhoto = imagesData.length-1;
@@ -80,14 +87,11 @@ rightArrow.onclick = () => {
     }
 };
 
-const thumbContainer = document.querySelector('#thumbnail-container');
-const thumbBox = document.querySelector('.thumbnail');
-
-let thumbnailIndex = 0;
-
+//thumbnails:
 for (let i = 0; i < imagesData.length; i++) {
     const thumbBox = document.createElement('div');
     thumbBox.setAttribute('class', 'thumbnail');
+    thumbBox.setAttribute('onclick', `loadPhoto(${i})`);
 
     const thumbTitle = document.createElement('p');
     thumbTitle.setAttribute('class', 'thumb-title');
@@ -96,17 +100,8 @@ for (let i = 0; i < imagesData.length; i++) {
     const thumbPhoto = document.createElement('img');
     thumbPhoto.setAttribute('class', 'thumbImage');
     thumbPhoto.setAttribute('src', imagesData[i].photo);
-    thumbPhoto.setAttribute('data-number', thumbnailIndex);
 
     thumbContainer.appendChild(thumbBox);
     thumbBox.appendChild(thumbTitle);
     thumbBox.appendChild(thumbPhoto);
-
-    thumbnailIndex++;
 }
-
-// thumbBox.onclick = (event) => {
-//     let index = parseInt($(event.target).attr('data-number'));
-//     loadPhoto(index);
-//     currentPhoto = index;
-// };

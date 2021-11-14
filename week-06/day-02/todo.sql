67384e4b926d7f6562fb967a1c1df80d94f01cb6
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-14 15:51:06 +0100

    finished sql todo exercise

---
+++ week-06/day-02/todo.sql
@@ -0,0 +1,20 @@
+SHOW DATABASES;
+
+CREATE DATABASE todo_app;
+USE todo_app;
+
+CREATE TABLE todos (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    task VARCHAR(255) NOT NULL,
+    isDone BOOLEAN NOT NULL DEFAULT false
+);
+
+INSERT INTO todos (task) VALUES ('Walk the dog'), ('Buy milk'), ('Do homework');
+SELECT * FROM todos;
+
+UPDATE todos
+SET isDone = true
+WHERE id = 2;
+
+DELETE FROM todos
+WHERE id = 3;

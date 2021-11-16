21b34cc6b2c6ca0f388abaaa2230b26e8e952bd4
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-16 13:16:57 +0100

    finished backgroundImg exercise

---
+++ week-07/day-02/background-switcher.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const htmlBody = document.querySelector('body');
+htmlBody.style.backgroundImage = `url('https://loremflickr.com/g/1280/800/hungary/')`;
+
+function updateImg(cache_ord) {
+  htmlBody.style.backgroundImage = `url('https://loremflickr.com/g/1280/800/hungary;sdfsdg=${cache_ord}')`;
+}
+
+setInterval(() => {
+  let ord = Math.random()*10000000000000000;
+  updateImg(ord);
+}, 5000);

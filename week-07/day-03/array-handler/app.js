ff8accfa6a00789e3d873bc0430fa651bb0aad66
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-17 16:55:08 +0100

    started array handler

---
+++ week-07/day-03/array-handler/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}`);
+});

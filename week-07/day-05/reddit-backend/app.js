cb3e2550d792be5aa9c23f32704cd02779d9ef27
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-19 15:37:19 +0100

    created get, post and put endpoints

--- week-07/day-05/reddit-backend/app.js
+++ week-07/day-05/reddit-backend/app.js
@@ -6,6 +6,8 @@ const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -29,5 +31,86 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/posts', (req, res) => {
+  const SQL_QUERY = 'SELECT * FROM posts;';
+  conn.query(SQL_QUERY, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json('INTERNAL SERVER ERROR');
+      return;
+    }
+    res.status(200).json({
+      "posts": rows});
+    });
+});
+
+app.post('/posts', (req, res) => {
+  const SQL_INSERT_QUERY = 'INSERT INTO posts (title, url, timestamp) VALUES (?, ?, ?);';
+
+  let newId = null;
+
+  conn.query(SQL_INSERT_QUERY, [req.body.title, req.body.url, Date.now()], (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json('INTERNAL SERVER ERROR');
+      return;
+    }
+
+    newId = rows.insertId;
+    const SQL_SELECT_QUERY = 'SELECT * FROM posts WHERE id=?';
+
+    conn.query(SQL_SELECT_QUERY, [newId], (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json('INTERNAL SERVER ERROR');
+        return;
+      }
+      res.status(200).json(rows);
+    });
+  });
+});
+
+app.put('/posts/:id/upvote', (req, res) => {
+  const SQL_UPDATE_QUERY = `UPDATE posts SET score = score + 1 WHERE id=?`;
+
+  conn.query(SQL_UPDATE_QUERY, [req.params.id], (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json('INTERNAL SERVER ERROR');
+      return;
+    }
+
+    const SQL_SELECT_QUERY = 'SELECT * FROM posts WHERE id=?';
+
+    conn.query(SQL_SELECT_QUERY, [req.params.id], (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json('INTERNAL SERVER ERROR');
+        return;
+      }
+      res.status(200).json(rows);
+    });
+  });
+});
+
+app.put('/posts/:id/downvote', (req, res) => {
+  const SQL_UPDATE_QUERY = `UPDATE posts SET score = score - 1 WHERE id=?`;
+
+  conn.query(SQL_UPDATE_QUERY, [req.params.id], (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json('INTERNAL SERVER ERROR');
+      return;
+    }
   
+    const SQL_SELECT_QUERY = 'SELECT * FROM posts WHERE id=?';
+  
+    conn.query(SQL_SELECT_QUERY, [req.params.id], (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json('INTERNAL SERVER ERROR');
+        return;
+      }
+      res.status(200).json(rows);
+    });
+  });
 });

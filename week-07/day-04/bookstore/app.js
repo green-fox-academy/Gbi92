dccf5026dbf55d5ef028f2cf959e8b3a21fdcda0
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-26 13:49:05 +0100

    changed avariable name to filter, because it fits better

--- week-07/day-04/bookstore/app.js
+++ week-07/day-04/bookstore/app.js
@@ -46,47 +46,47 @@ app.get('/books', (req, res) => {
     JOIN category AS c ON b.cate_id = c.cate_id
     JOIN newpublisher AS p ON b.pub_id = p.pub_id`;
 
-  const params = [];
+  const filter = [];
 
   if (Object.keys(req.query).length > 0) {
     SQL_QUERY += ' WHERE ';
   }
 
   if (req.query.category) {
-    if (params.length > 0) {
+    if (filter.length > 0) {
       SQL_QUERY += 'AND ';
     } 
     SQL_QUERY += 'c.cate_descrip=? ';
-    params.push(req.query.category);
+    filter.push(req.query.category);
   }
 
   if (req.query.publisher) {
-    if (params.length > 0) {
+    if (filter.length > 0) {
       SQL_QUERY += 'AND ';
     } 
     SQL_QUERY += 'p.pub_name=? ';
-    params.push(req.query.publisher);
+    filter.push(req.query.publisher);
   }
 
   if (req.query.plt) {
-    if (params.length > 0) {
+    if (filter.length > 0) {
       SQL_QUERY += 'AND ';
     } 
     SQL_QUERY += 'b.book_price<? ';
-    params.push(req.query.plt);
+    filter.push(req.query.plt);
   }
 
   if (req.query.pgt) {
-    if (params.length > 0) {
+    if (filter.length > 0) {
       SQL_QUERY += 'AND ';
     } 
     SQL_QUERY += 'b.book_price>? ';
-    params.push(req.query.pgt);
+    filter.push(req.query.pgt);
   }
 
   SQL_QUERY += 'ORDER BY b.book_name;';
 
-  conn.query(SQL_QUERY, params, (error, rows) => {
+  conn.query(SQL_QUERY, filter, (error, rows) => {
     if(error) {
       console.log(error);
       res.status(500).json('INTERNAL SERVER ERROR');

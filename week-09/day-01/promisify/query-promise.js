96cc91b3c1a6c279f21facafc11ecc8792d7e8b8
Gabriella Koos <koos.gabriella92@gmail.com>
2021-11-29 16:41:38 +0100

    finished query promisify

---
+++ week-09/day-01/promisify/query-promise.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mysql = require('mysql');
+
+function mysqlQuery(query) {
+  return new Promise((resolve, reject) => {
+    conn.query(query, (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      } else {
+        return resolve(rows);
+      }
+    })
+  })
+}
+
+let results = await mysqlQuery('SELECT * FROM users');

476baa0d6bf2d7899942d84fdfba12707a03e3a0
Gabriella Koos <koos.gabriella92@gmail.com>
2021-12-02 17:30:35 +0100

    started fetch

--- week-07/day-05/reddit-backend/public/js/add-post.js
+++ week-07/day-05/reddit-backend/public/js/add-post.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (form.title.value === '' || form.url.value) {
+    alert('Please fill all the fields');
+    return;
+  }
+
+  fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json', 'user_id' : 2},
+    body: JSON.stringify({
+      title: form.title.value,
+      url: form.url.value
+    })
+  })
+  .then(res => res.json())
+  .then(data => data)
+});

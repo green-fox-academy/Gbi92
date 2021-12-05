7ae190ca067b369735dcdb8005943528fd813108
Gabriella Koos <koos.gabriella92@gmail.com>
2021-12-05 13:50:48 +0100

    image state can change on click

--- week-07/day-01/giphy-fetch.js
+++ week-07/day-01/giphy-fetch.js
@@ -3,15 +3,20 @@
 const htmlBody = document.querySelector('body');
 const limit = 16;
 
-fetch('https://api.giphy.com/v1/gifs/search?api_key=B3IklSivzPUcPsYki5lnGXOXJYSHf0Wf&q=cute&limit=16')
+fetch(`https://api.giphy.com/v1/gifs/search?api_key=B3IklSivzPUcPsYki5lnGXOXJYSHf0Wf&q=cute&limit=${limit}`)
   .then(response => response.json())
   .then(data => {
+    console.log(data);
     for (let i = 0; i < limit; i++) {
       const img = document.createElement('img');
       img.setAttribute('src', data.data[i].images.fixed_width_still.url);
-      img.onclick = () => {
-        img.setAttribute('src', data.data[i].images.fixed_width.url)
-      };
       htmlBody.appendChild(img);
+      img.onclick = () => {
+        if (img.getAttribute('src') === data.data[i].images.fixed_width_still.url){
+          img.setAttribute('src', data.data[i].images.fixed_width.url)
+        } else {
+          img.setAttribute('src', data.data[i].images.fixed_width_still.url);
+        }
+      };
     }
   });

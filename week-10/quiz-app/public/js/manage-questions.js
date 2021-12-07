bc92f5d600745ce83c2fabee7f933eda11b0e1b0
Gabriella Koos <koos.gabriella92@gmail.com>
2021-12-07 17:30:15 +0100

    started posting question feature

--- week-10/quiz-app/public/js/manage-questions.js
+++ week-10/quiz-app/public/js/manage-questions.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const container = document.querySelector('.container');
+const form = document.querySelector('form');
 
 fetch('http://localhost:8080/api/questions')
 .then(res => res.json())
@@ -34,4 +35,36 @@ container.addEventListener('click', (event) => {
     .then(res => res.json())
     .then(data => window.location.href = 'http://localhost:8080/questions')
   }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  fetch('http://localhost:8080/api/questions', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: {
+      question: form.newQuestion.value,
+      answers: [
+        {
+          answer_1: form.one.value,
+          is_correct: form.answerOne.checked ? 1 : 0
+        },
+        {
+          answer_1: form.two.value,
+          is_correct: 0
+        },
+        {
+          answer_1: form.three.value,
+          is_correct: 0
+        },
+        {
+          answer_1: form.four.value,
+          is_correct: 0
+        }
+      ]
+    }
+  })
+  .then(res => res.json())
+  .then(data => window.location.href = 'http://localhost:8080/questions')
 });

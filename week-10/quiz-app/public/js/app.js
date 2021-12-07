cd71f93f6dde16a1286780fb7b01f02a588e6189
Gabriella Koos <koos.gabriella92@gmail.com>
2021-12-07 15:32:43 +0100

    created frontend of the game

---
+++ week-10/quiz-app/public/js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const question = document.querySelector('.question');
+const scoreBoard = document.querySelector('.score');
+const answerContainer = document.querySelector('.answers');
+
+let score = 0;
+
+function fetchQuestion() {
+  fetch('http://localhost:8080/api/game')
+  .then(res => res.json())
+  .then(data => {
+    question.innerText = data.question;
+    data.answers.forEach((answer, i) => {
+      let currentAnswer = document.querySelector(`.no${i+1}`);
+      currentAnswer.innerText = answer['answer_' + (i+1)];
+      currentAnswer.setAttribute('data-correctness', answer.is_correct);
+      currentAnswer.classList.remove('correct');
+      currentAnswer.classList.remove('wrong');
+    });
+  })
+  .catch((error) => console.log(error))
+}
+
+window.addEventListener('load', () => {
+  fetchQuestion();
+});
+
+answerContainer.addEventListener('click', (event) => {
+  if (Number(event.target.dataset.correctness) === 1) {
+    score += 1;
+    event.target.classList.add('correct');
+    scoreBoard.innerText = `SCORE: ${score}`;
+  } else {
+    event.target.classList.add('wrong');
+  }
+  setTimeout(fetchQuestion, 1000);
+});

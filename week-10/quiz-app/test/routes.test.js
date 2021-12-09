cdd15481ce6f2a25fbb3dfb22dade2e37e083a0d
Gabriella Koos <koos.gabriella92@gmail.com>
2021-12-09 14:29:11 +0100

    tested post endpoint

--- week-10/quiz-app/test/routes.test.js
+++ week-10/quiz-app/test/routes.test.js
@@ -12,10 +12,45 @@ describe('Quiz game webapp', () => {
         .get('/api/game')
         .expect(200)
         .end((err, response) => {
-          if (err) {
-            throw err;
-          }
+          expect(err).to.be.null;
           expect(response.body.answers.length).to.equal(4);
+          done();
+        })
+    })
+  })
+
+  describe('POST /api/questions', () => {
+    it('should save the new question and possible answers and return a message', (done) => {
+      let newQuestion = {
+        question: 'Test question',
+        answers: [
+          {
+            answer_1: 'test01',
+            is_correct: 0
+          },
+          {
+            answer_2: 'test02',
+            is_correct: 0
+          },
+          {
+            answer_3: 'test03',
+            is_correct: 1
+          },
+          {
+            answer_4: 'test04',
+            is_correct: 0
+          }
+        ]
+      };
+
+      request(app)
+        .post('/api/questions')
+        .send(newQuestion)
+        .expect(201)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.body.message).to.be.equal('Question has been added to quiz');
+
           done();
         })
     })
